5d8a52e7e1f0cc369b5f3f867bacf72a63a50664
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-11-29 14:55:31 -0500

    Fixed: Error in services added to server

--- 2GetherLearnAPI/Program.cs
+++ 2GetherLearnAPI/Program.cs
@@ -4,15 +4,6 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
 var Configuration = builder.Configuration;
 
 // MongoDB Connection
@@ -24,6 +15,15 @@ sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
 
 // Setting Services
 builder.Services.AddScoped<PostService>();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
